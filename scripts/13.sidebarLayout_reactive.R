dddab2ce7e1150e303f444d1b7a490972c60027f
tonyabraham4 <tonyabraham4@gmail.com>
2019-04-09 21:57:56 +0530

    reactive

---
+++ scripts/13.sidebarLayout_reactive.R
@@ -0,0 +1,80 @@
+# Add reactive data frame
+# We ended the previous chapter with an app that allows you to download a data file with selected variables from the movies dataset. 
+# We will now extend this app by adding a table output of the selected data as well. Given that the same dataset will be used in two outputs, 
+# it makes sense to make our code more efficient by using a reactive data frame.
+
+
+
+library(shiny)
+library(dplyr)
+library(readr)
+load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"))
+
+# UI
+ui <- fluidPage(
+  sidebarLayout(
+    
+    # Input(s)
+    sidebarPanel(
+      
+      # Select filetype
+      radioButtons(inputId = "filetype",
+                   label = "Select filetype:",
+                   choices = c("csv", "tsv"),
+                   selected = "csv"),
+      
+      # Select variables to download
+      checkboxGroupInput(inputId = "selected_var",
+                         label = "Select variables:",
+                         choices = names(movies),
+                         selected = c("title"))
+      
+    ),
+    
+    # Output(s)
+    mainPanel(
+      HTML("Select filetype and variables, then download and/or view the data."),
+      br(), br(),
+      downloadButton(outputId = "download_data", label = "Download data"),
+      br(), br(),
+      DT::dataTableOutput(outputId = "moviestable")
+    )
+  )
+)
+
+# Server
+server <- function(input, output) {
+  
+  # Create reactive data frame
+  movies_selected <- reactive({
+    ___ # ensure input$selected_var is available
+    ___ # select columns of movies
+  })
+  
+  # Create data table
+  output$moviestable <- DT::renderDataTable({
+    req(input$selected_var)
+    DT::datatable(data = movies %>% select(input$selected_var), 
+                  options = list(pageLength = 10), 
+                  rownames = FALSE)
+  })
+  
+  # Download file
+  output$download_data <- downloadHandler(
+    filename = function() {
+      paste0("movies.", input$filetype)
+    },
+    content = function(file) { 
+      if(input$filetype == "csv"){ 
+        write_csv(movies %>% select(input$selected_var), file) 
+      }
+      if(input$filetype == "tsv"){ 
+        write_tsv(movies %>% select(input$selected_var), file) 
+      }
+    }
+  )
+  
+}
+
+# Create a Shiny app object
+shinyApp(ui = ui, server = server)
